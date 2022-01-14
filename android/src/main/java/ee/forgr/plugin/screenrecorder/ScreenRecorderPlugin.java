97e15b5f3a81a68dfbbdd3c19e00f3f3231f2e55
martindonadieu <martindonadieu@gmail.com>
2022-01-14 13:46:50 +0000

    fix: android version

--- android/src/main/java/ee/forgr/plugin/screenrecorder/ScreenRecorderPlugin.java
+++ android/src/main/java/ee/forgr/plugin/screenrecorder/ScreenRecorderPlugin.java
@@ -1,6 +1,5 @@
 package ee.forgr.plugin.screenrecorder;
 
-import com.getcapacitor.JSObject;
 import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
 import com.getcapacitor.PluginMethod;
@@ -9,17 +8,17 @@ import com.getcapacitor.annotation.CapacitorPlugin;
 @CapacitorPlugin(name = "ScreenRecorder")
 public class ScreenRecorderPlugin extends Plugin {
 
-    private ScreenRecorder implementation = new ScreenRecorder();
+    val recorder = ScrCast.use(this.bridge.getActivity());
 
     @PluginMethod
     public void start(PluginCall call) {
-        implementation.start();
+        recorder.record();
         call.resolve();
     }
     
     @PluginMethod
     public void stop(PluginCall call) {
-        implementation.stop();
+        recorder.stopRecording();
         call.resolve();
     }
 }
