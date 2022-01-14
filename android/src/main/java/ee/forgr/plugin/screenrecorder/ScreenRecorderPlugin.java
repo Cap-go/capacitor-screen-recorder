798663fa08d525c33c3eb4b46009bfc0b20702b8
martindonadieu <martindonadieu@gmail.com>
2022-01-14 14:02:31 +0000

    fix: type in android

--- android/src/main/java/ee/forgr/plugin/screenrecorder/ScreenRecorderPlugin.java
+++ android/src/main/java/ee/forgr/plugin/screenrecorder/ScreenRecorderPlugin.java
@@ -4,11 +4,12 @@ import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
 import com.getcapacitor.PluginMethod;
 import com.getcapacitor.annotation.CapacitorPlugin;
+import dev.bmcreations.scrcast.ScrCast;
 
 @CapacitorPlugin(name = "ScreenRecorder")
 public class ScreenRecorderPlugin extends Plugin {
 
-    val recorder = ScrCast.use(this.bridge.getActivity());
+    ScrCast recorder = ScrCast.use(this.bridge.getActivity());
 
     @PluginMethod
     public void start(PluginCall call) {
