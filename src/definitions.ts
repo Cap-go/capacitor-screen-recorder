8f5d683ceb6b2d628cab3c0bb8b3d41398294d43
Martin Donadieu <martindonadieu@gmail.com>
2025-02-27 00:59:19 +0000

    fix: definition

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,12 +1,11 @@
 export interface ScreenRecorderPlugin {
   /**
    * start the recording
-   *
+   * @param options Recording options
    */
-  start(): Promise<void>;
+  start(options?: { recordAudio?: boolean }): Promise<void>;
   /**
    * stop the recording
-   *
    */
   stop(): Promise<void>;
 }
