743758e9d7bfff8c54cb53e15b157043855c60a6
Martin Donadieu <martindonadieu@gmail.com>
2026-06-07 12:27:14 +0200

    ci: deploy example app to capgo

---
+++ example-app/capacitor.config.ts
@@ -0,0 +1,24 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+import pkg from './package.json';
+
+const config: CapacitorConfig = {
+  "appId": "app.capgo.screen.recorder",
+  "appName": "Screen Recorder Example",
+  "webDir": "dist",
+  "plugins": {
+    "SplashScreen": {
+      "launchAutoHide": false
+    },
+    "CapacitorUpdater": {
+      "appId": "app.capgo.screen.recorder",
+      "autoUpdate": true,
+      "autoSplashscreen": true,
+      "directUpdate": "always",
+      "defaultChannel": "production",
+      "version": pkg.version
+    }
+  }
+};
+
+export default config;
--- example-app/src/main.js
+++ example-app/src/main.js
@@ -1,3 +1,5 @@
+import { CapacitorUpdater } from '@capgo/capacitor-updater';
+import { Capacitor } from '@capacitor/core';
 
 import './style.css';
 import { ScreenRecorder } from '@capgo/capacitor-screen-recorder';
@@ -182,3 +184,9 @@ runButton.addEventListener('click', async () => {
 });
 
 populateActions();
+
+if (Capacitor.isNativePlatform()) {
+  CapacitorUpdater.notifyAppReady().catch((error) => {
+    console.error('Capgo notifyAppReady failed', error);
+  });
+}
